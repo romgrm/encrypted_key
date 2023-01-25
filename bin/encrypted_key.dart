b319dc0ea02564faeb8d3436d066f24dacb7bd53
Romain Gréaume <r.greaume@beapp.fr>
2023-01-25 20:26:39 +0100

    feat: remove useless code and impl EncryptedKeyCommandRunner call

--- bin/encrypted_key.dart
+++ bin/encrypted_key.dart
@@ -1,17 +1,6 @@
-import 'package:encrypted_key/encrypted_key.dart' as encrypted_key;
-import 'package:args/args.dart';
-import 'dart:convert';
-import 'dart:io';
+import 'package:encrypted_key/src/command_runner.dart';
 
 const valueToEncrypt = 'value-to-encrypt';
 void main(List<String> arguments) {
-  final parser = ArgParser()..addFlag(valueToEncrypt, negatable: false, abbr: 'v');
-
-  ArgResults argResults = parser.parse(arguments);
-  final paths = argResults.rest;
-
-  print("arguments from results : $paths");
-  print("rest from results : ${argResults.rest}");
-
-  print('Your input ${arguments.first} was encrypted to : ${encrypted_key.encrypted(arguments.first)}!');
+  EncryptedKeyCommandRunner().run(arguments);
 }
