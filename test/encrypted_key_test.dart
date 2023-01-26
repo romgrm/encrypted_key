50e72d7fd636b6b9b715215049528fe8324f0982
Romain Gréaume <r.greaume@beapp.fr>
2023-01-26 10:35:54 +0100

    refactor: remove useless code test

--- test/encrypted_key_test.dart
+++ test/encrypted_key_test.dart
@@ -1,8 +1,1 @@
-// import 'package:encrypted_key/encrypted_key.dart';
-// import 'package:test/test.dart';
 
-// void main() {
-//   test('calculate', () {
-//     expect(calculate(), 42);
-//   });
-// }
